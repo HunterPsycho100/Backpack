a59edec2e58ae534c99a7c9aa8d31b5f2ca2d2f6
HunterPsycho100 <67747295+HunterPsycho100@users.noreply.github.com>
2021-01-25 00:42:31 -0800

    finished back methods
    added all the methods to backpack

--- Program.cs
+++ Program.cs
@@ -17,45 +17,94 @@ namespace Backpack
         }
 
         //method to add items to the main pocket
-        public bool InsertItemsInMainPocket (double items)
+        public bool InsertItemsInMainPocket (double itemWeight)
         {
-
+            if(itemWeight > 0 && mainPocketWeight + itemWeight <= 10)
+            {
+                mainPocketWeight += itemWeight;
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
         }
 
         //method to add items to the right pocket
-        public bool InsertItemsInRightPocket (double items)
+        public bool InsertItemsInRightPocket (double itemWeight)
         {
-
+            if (itemWeight > 0 && rightPocketWeight + itemWeight <= 5)
+            {
+                rightPocketWeight += itemWeight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         //method to add items to the left pocket
-        public bool InsertItemsInLeftPocket (double items)
+        public bool InsertItemsInLeftPocket (double itemWeight)
         {
-
+            if (itemWeight > 0 && leftPocketWeight + itemWeight <= 5)
+            {
+                leftPocketWeight += itemWeight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         //returns the total sum of each pocket weight
         public double GetTotalWeight()
         {
-
+            return mainPocketWeight + leftPocketWeight + rightPocketWeight;
         }
 
         //method to remove items from the main pocket
-        public bool RemoveitemsFromMainPocket (double items)
+        public bool RemoveitemsFromMainPocket (double itemWeight)
         {
-
+             if (itemWeight > 0 && mainPocketWeight - itemWeight > 0)
+            {
+                mainPocketWeight -= itemWeight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }       
         }
 
         //method to remove items from the right pocket
-        public bool RemoveitemsFromRightPocket(double items)
+        public bool RemoveitemsFromRightPocket(double itemWeight)
         {
-
+            if (itemWeight > 0 && rightPocketWeight - itemWeight > 0)
+            {
+                rightPocketWeight -= itemWeight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }        
+        
         }
 
         //method to remove items from the left pocket
-        public bool RemoveitemsFromLeftPocket(double items)
+        public bool RemoveitemsFromLeftPocket(double itemWeight)
         {
-
+            if (itemWeight > 0 && leftPocketWeight - itemWeight > 0)
+            {
+                leftPocketWeight -= itemWeight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         static void Main(string[] args)
         {
