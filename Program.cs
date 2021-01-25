a1f613f88a3a97b3a18533352f3bef1c6a59ccde
HunterPsycho100 <67747295+HunterPsycho100@users.noreply.github.com>
2021-01-25 00:06:32 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Backpack
+{
+    class Backpack
+    {
+        private double mainPocketWeight;
+        private double leftPocketWeight;
+        private double rightPocketWeight;
+
+        //contructure to initialize fields
+        public Backpack()
+        {
+            mainPocketWeight = 0;
+            leftPocketWeight = 0;
+            rightPocketWeight = 0;
+        }
+
+        //method to add items to the main pocket
+        public bool InsertItemsInMainPocket (double items)
+        {
+
+        }
+
+        //method to add items to the right pocket
+        public bool InsertItemsInRightPocket (double items)
+        {
+
+        }
+
+        //method to add items to the left pocket
+        public bool InsertItemsInLeftPocket (double items)
+        {
+
+        }
+
+        //returns the total sum of each pocket weight
+        public double GetTotalWeight()
+        {
+
+        }
+
+        //method to remove items from the main pocket
+        public bool RemoveitemsFromMainPocket (double items)
+        {
+
+        }
+
+        //method to remove items from the right pocket
+        public bool RemoveitemsFromRightPocket(double items)
+        {
+
+        }
+
+        //method to remove items from the left pocket
+        public bool RemoveitemsFromLeftPocket(double items)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Backpack myBackPack = new Backpack(); //creates a backpack object
+            Console.WriteLine(myBackPack.InsertItemsInMainPocket(8));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
